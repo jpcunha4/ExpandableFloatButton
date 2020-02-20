df5fe7ec50d426208bda0550b8a5f79dc4a9cfd3
João Paulo Cunha Ávila <jpcunha4@gmail.com>
2020-02-20 18:36:15 -0300

    Initial Commit

---
+++ FloatingMenu/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FloatingMenu
+//
+//  Created by Joao Paulo Cunha Ávila on 20/02/20.
+//  Copyright © 2020 Joao Paulo Cunha Ávila. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
